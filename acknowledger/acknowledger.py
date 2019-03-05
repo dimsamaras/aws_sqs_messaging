455a007fbafab138fbc6faf80df7944f93c16abc
dimsam <dimitris.samaras@Schoox.com>
2019-03-05 19:26:25 +0200

    cloudwatch agent per minute

--- acknowledger/acknowledger.py
+++ acknowledger/acknowledger.py
@@ -2,7 +2,7 @@
 
 import threading
 import logger
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class ackThread(threading.Thread):
 
@@ -26,7 +26,8 @@ def ack_messages(thread):
 	"""Pull messages from intemediate queue and acknowledge them."""
 
 	delete_batch = []
-	metrics = []
+	metrics = reset_metrics_dict()
+	timer = datetime.now()
 	while not thread.stopEvent.isSet():
 		if not thread.ackQueue.empty():
 			delete_batch.append(thread.ackQueue.get_nowait())
@@ -39,17 +40,17 @@ def ack_messages(thread):
 				thread.ackQueue.task_done()
 
 				if len(delete_batch) == thread.delete_batch_max:
-					logger.logging.info('Will delete on stop = ' + " ".join(str(x) for x in delete_batch))
-					delete_batch = send_ack(thread.SqsManager, delete_batch)
-					metrics = calculate_metrics(metrics, delete_batch, thread.metrics_batch_max)
+					# logger.logging.info('Will delete on stop = ' + " ".join(str(x) for x in delete_batch))
+					delete_batch, metrics, timer = calculate_metrics(metrics, delete_batch, timer, thread.CWManager, thread.SqsManager, thread.metrics_batch_max)
+					delete_batch = send_ack(thread.SqsManager, delete_batch)					
 			if delete_batch:
-				logger.logging.info('Will delete on stop final remaining = ' + " ".join(str(x) for x in delete_batch))
+				# logger.logging.info('Will delete on stop final remaining = ' + " ".join(str(x) for x in delete_batch))
+				delete_batch, metrics, timer = calculate_metrics(metrics, delete_batch, timer, thread.CWManager, thread.SqsManager, thread.metrics_batch_max, True)
 				delete_batch = send_ack(thread.SqsManager, delete_batch)
-				metrics = calculate_metrics(metrics, delete_batch, thread.metrics_batch_max, true)
 		elif len(delete_batch) == thread.delete_batch_max:
-			logger.logging.info('Will delete = ' + " ".join(str(x) for x in delete_batch))
+			# logger.logging.info('Will delete = ' + " ".join(str(x) for x in delete_batch))
+			delete_batch, metrics, timer  = calculate_metrics(metrics, delete_batch, timer, thread.CWManager, thread.SqsManager, thread.metrics_batch_max)
 			delete_batch = send_ack(thread.SqsManager, delete_batch)
-			metrics = calculate_metrics(metrics, delete_batch, thread.metrics_batch_max)
 
 def send_ack(SqsManager, messages):
 	"""Send message acknowledgement  in batches."""
@@ -58,7 +59,9 @@ def send_ack(SqsManager, messages):
 
 	return []
 
-def send_metrics(CWManager, metrics):
+def send_metrics(CWManager, SqsManager, metrics):
+	logger.logging.info('Calculated metrics =' + str(metrics))
+
 	CWManager.put_metric_data('Schoox', [
 											{
 												'MetricName': 'ExecutionTime',
@@ -84,26 +87,30 @@ def send_metrics(CWManager, metrics):
 											},
 										])
 	 
-
-def calculate_metrics(metrics, messages, metrics_batch_max, force=false):
+def calculate_metrics(metrics, messages, timer, CWManager, SqsManager, metrics_batch_max, force=False):
 	"""Calculate message processing metrics"""
 
 	for msg in messages:
-		entryExec = message['ProcTime']
+		entryExec = int(msg['ProcTime'])
+		try:
+			del msg['ProcTime']
+		except KeyError:
+			pass
 		metrics['max'] = entryExec if metrics['count'] == 0 else entryExec if metrics['max'] < entryExec else metrics['max']
 		metrics['min'] = entryExec if metrics['count'] == 0 else entryExec if metrics['min'] > entryExec else metrics['min']
 		metrics['count'] += 1
-		metrics['sum']   += entryExec
+		metrics['sum'] += entryExec
 	
-		if $metrics['count'] == metrics_batch_max:
-			send_metrics(CWManager, metrics)
-			metrics = []
+	elapsed = datetime.now() - timer
+	if (elapsed > timedelta(minutes=1)) and (metrics['count']>0):
+		send_metrics(CWManager, SqsManager, metrics)
+		metrics = reset_metrics_dict()
 
-		logger.logging.info('Check metrics = {}' + " ".format(metrics))
-	
-	if force:
-		send_metrics(CWManager, metrics)
+	if force and (metrics['count']>0):
+		send_metrics(CWManager, SqsManager, metrics)
 
-	return metrics
+	return messages, metrics, timer 
 
 
+def reset_metrics_dict():
+	return {'max':0, 'sum':0, 'min':0, 'count':0}
